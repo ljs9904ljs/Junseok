95e98387cb803e40f99e34494511293521bd318b
ljs9904ljs <ljs9904ljs@postech.ac.kr>
2019-12-27 19:18:23 +0900

    Create codingTest2.c

---
+++ C_C++_Projects/C_files/codingTest2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+
+int main() {
+    int budget, teams;
+    int limit;
+    int* req;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
